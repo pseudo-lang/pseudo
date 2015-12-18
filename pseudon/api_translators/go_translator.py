30dd38450ba34a97e6dba4bd5acb80965205d740
Alexander Ivanov <alehander42@gmail.com>
2015-12-18 23:14:57 +0200

    WIP 2

--- pseudon/api_translators/go_translator.py
+++ pseudon/api_translators/go_translator.py
@@ -1,6 +1,6 @@
 from pseudon.types import *
 from pseudon.api_translator import ApiTranslator
-from pseudon.pseudon_tree import Node, method_call, call
+from pseudon.pseudon_tree import Node, method_call, call, if_statement, foreach, item_assignment, assignment
 
 
 class GoTranslator(ApiTranslator):
@@ -21,6 +21,25 @@ class GoTranslator(ApiTranslator):
     def expand_insert(receiver, index, element):
         return call('append', [])
 
+    @staticmethod
+    def expand_map(receiver, function, assignment):
+        if function.type == 'lambda':
+            iter = function.args[0]
+        else:
+            iter = 'element'
+        return [Node('_go_assignment',
+            name=local('result'),
+            value=Node('_make_slice',
+                type=receiver.pseudon_type,
+                length=call('len', receiver))),
+            foreach(
+                [iter, 'j'],
+                receiver, [
+                    item_assignment(result,
+                        'j',
+                        call(function, [iter]))]),
+            'result']
+
     api = {
         'List': {
             '@equivalent':  'slice',
@@ -34,6 +53,8 @@ class GoTranslator(ApiTranslator):
         'Dictionary': {
         },
         'Enumerable': {
+            'map':          expand_map,
+            'filter':       expand_filter
         },
 
         'Int': {
