e468c11f952dc4072190f4ae0ccdcb8d672fedc9
Alexander Ivanov <alehander42@gmail.com>
2016-03-11 09:16:28 +0200

    Support dicts in generate

--- pseudo/__init__.py
+++ pseudo/__init__.py
@@ -1,4 +1,18 @@
-'''pseudo is translating asts'''
+'''
+Pseudo: a library for idiomatic code generation
+
+Pseudo is a library for generating code 
+in different high level languages
+and a system for language translation: 
+its goal is to be able to translate any code expressed in a 
+certain "pseudo-translateable" subset of each supported language
+or as a pseudo AST to readable and idiomatic code in any of 
+the supported target languages. 
+
+Supported target languages currently are 
+Python, Ruby, C#, JavaScript, Go 
+'''
+
 import pseudo.api_translators
 import pseudo.api_translators.ruby_translator
 import pseudo.api_translators.python_translator
@@ -14,6 +28,9 @@ import pseudo.generators.js_generator
 import pseudo.generators.csharp_generator
 import pseudo.generators.cpp_generator
 import pseudo.generators.golang_generator
+
+import pseudo.loader
+import pseudo.pseudo_tree
 
 SUPPORTED_FORMATS = {'js', 'javascript', 'py', 'python', 'rb', 'ruby', 'go', 'golang', 'cs', 'csharp', 'cpp'}
 FILE_EXTENSIONS = {'js': 'js', 'javascript': 'js', 'py': 'py', 'python': 'py', 'rb': 'rb', 'ruby': 'rb', 'go': 'go', 'golang': 'go', 'cs': 'cs', 'csharp': 'cs', 'cpp': 'cpp'}
@@ -38,10 +55,52 @@ GENERATORS = {
     for format in SUPPORTED_FORMATS
 }
 
+def generate_main(main, language):
+    '''
+    generate output code for main in `language`
+
+    `main` is a dict/Node or a list of dicts/Nodes with pseudo ast
+
+    e.g.
+    > print(generate_main({'type': 'int', 'value': 0, 'pseudo_type': 'Int'}, 'rb'))
+    2
+    > print(generate_main([pseudo.pseudo_tree.to_node('a'), pseudo.pseudo_tree.to_node(0)], 'js'))
+    'a';
+    0;
+    '''
+    base = {'type': 'module', 'custom_exceptions': [], 'definitions': [], 'constants': [], 'main': [], 'pseudo_type': 'Void'}
+    base_node = pseudo.loader.convert_to_syntax_tree(base)
+    if isinstance(main, dict):
+        base['main'] = [main]
+    elif isinstance(main, list):
+        if main and isinstance(main[0], dict):
+            base['main'] = main
+        else:
+            base_node.main = main
+    elif isinstance(main, pseudo.pseudo_tree.Node):
+        base_node.main = [main]
+    if base['main']:
+        q = pseudo.loader.convert_to_syntax_tree(base)
+    else:
+        q = base_node
+    return generate(q, language)
+
 
 def generate(pseudo_ast, language):
-    '''generate output code in the given language'''
+    '''
+    generate output code in `language`
+
+    `pseudo_ast` can be a plain `dict` with ast data or
+    it can use the internal `pseudo` `Node(type, **fields)` format
+
+    if you want to play with it, you can use `generate_main` which 
+    expects just a dict node / a list of dict nodes and a language
+
+    `language` can be 'py', 'python', 'rb', 'ruby',
+      'javascript', 'js', 'cs', 'csharp', 'go' or 'cpp'
+    '''
+
+    if isinstance(pseudo_ast, dict):
+        pseudo_ast = pseudo.loader.convert_to_syntax_tree(pseudo_ast)
     translated_ast = API_TRANSLATORS[language](pseudo_ast).api_translate()
-    # print(language, API_TRANSLATORS[language].functions['io']['display'])
-    # input()
     return GENERATORS[language]().generate(translated_ast)
--- setup.py
+++ setup.py
@@ -6,20 +6,27 @@ except ImportError:
 
 setup(
     name='pseudo',
-    version='0.2',
+    version='0.2.1',
     description='a framework for high level code generation',
     author='Alexander Ivanov',
     author_email='alehander42@gmail.com',
     url='https://github.com/alehander42/pseudo',
-    download_url='https://github.com/alehander42/pseudo/tarball/0.2',
+    download_url='https://github.com/alehander42/pseudo/archive/v0.2.tar.gz',
     keywords=['compiler', 'generation', 'c++', 'ruby', 'c#', 'javascript', 'go', 'python', 'transpiler'],
-    packages=['pseudo'],
-    license='MIT License',
+    packages=['pseudo', 'pseudo.api_translators', 'pseudo.middlewares', 'pseudo.generators'],
+    license='MIT',
     install_requires=[
-        'PyYAML==3.11'
+        'PyYAML'
     ],
     classifiers=[
+        'Development Status :: 4 - Beta',
         'Programming Language :: Python',
-        'Programming Language :: JavaScript'
+        'Programming Language :: JavaScript',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
     ],
 )
